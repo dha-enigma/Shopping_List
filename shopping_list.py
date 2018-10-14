69b64a05dc9f84687f77e918c4ec873bf77451bf
dha-enigma <valliant2145@gmail.com>
2018-10-14 23:10:22 +0100

    Update shopping_list.py
    Added functionality by using functions and cleaning up code so its not only easily readable but also easy to modify

--- shopping_list.py
+++ shopping_list.py
@@ -1,9 +1,28 @@
 #create a list to hold our items
 shopping_list = []
 
-#Instructions for using the app
-print("what should we pick up at the store?")
-print("enter 'DONE' to stop adding items")
+def show_help():
+    #Instructions for using the app
+    print("what should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items
+Enter 'HELP' for this help
+Enter 'SHOW' to see your current list
+""")
+
+def show_list():
+    #print list items
+    print("Here's your list: ")
+
+    for item in shopping_list:
+         print(item)
+
+def add_newItem(newItem):
+    #add new items to our list
+    shopping_list.append(newItem)
+    print("Added {}. Shopping list now has {} items".format(newItem, len(shopping_list)))
+
+show_help()
 
 #ask for new items
 while True : 
@@ -12,10 +31,13 @@ while True :
     #be able to quit the app
     if newItem == 'DONE':
         break
-    #add new items to our list
-    shopping_list.append(newItem)
+    elif newItem == 'HELP':
+        show_help()
+        continue
+    elif newItem == 'SHOW':
+        show_list()
+        continue
+    add_newItem(newItem)
+    
 
-#print list items
-print("Here's your list: ")
-for item in shopping_list:
-    print(item)
+show_list()
